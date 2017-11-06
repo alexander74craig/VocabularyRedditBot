7b34859a20443a4d00449dd6b40f16613aa79f59
Craig <alex.craig@lamresearch.com>
2017-11-06 11:00:15 -0800

    refactoring and testing apiCall.py

--- apiCall.py
+++ apiCall.py
@@ -1,6 +1,11 @@
 import praw
 
-redditInstance= praw.Reddit('vocab' , user_agent='Python/PC:vocabularyredditbot:v0.1')
-zachBraff= redditInstance.redditor('zachinoz')
-for comment in zachBraff.comments.new(limit=None):
-    print( comment.body)
+def getUserComments(username, commentLimit):
+    redditInstance= praw.Reddit('vocab' , user_agent='Python/PC:vocabularyredditbot:v0.1')
+    zachBraff= redditInstance.redditor(username)
+    if (commentLimit<1):
+        commentLimit=None
+    comments=[]
+    for comment in zachBraff.comments.new(limit=commentLimit):
+        comments.append(comment.body)
+    return comments
