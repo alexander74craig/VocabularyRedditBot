b8f788189e9361ef485a3354764091eaa245654d
Craig <alex.craig@lamresearch.com>
2017-11-06 13:59:56 -0800

    adding functionality for retreiving mentions and replying to mentions

--- apiCall.py
+++ apiCall.py
@@ -2,10 +2,22 @@ import praw
 
 def getUserComments(username, commentLimit):
     redditInstance= praw.Reddit('vocab' , user_agent='Python/PC:vocabularyredditbot:v0.1')
-    zachBraff= redditInstance.redditor(username)
+    user= redditInstance.redditor(username)
     if (commentLimit<1):
         commentLimit=None
     comments=[]
-    for comment in zachBraff.comments.new(limit=commentLimit):
+    for comment in user.comments.new(limit=commentLimit):
         comments.append(comment.body)
     return comments
+
+def getMentions():
+    redditInstance= praw.Reddit('vocab' , user_agent='Python/PC:vocabularyredditbot:v0.1')
+    mentions=[]
+    for mention in redditInstance.inbox.mentions(limit=None):
+        mentions.append(mention)
+    return mentions
+
+def replyToMention(mention, message):
+    redditInstance= praw.Reddit('vocab' , user_agent='Python/PC:vocabularyredditbot:v0.1')
+    mention.reply(message)
+
