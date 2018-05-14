9bfff0ce376ef2936c83f49754daab1d7b49e2c6
alexander74craig <alexander74craig@gmail.com>
2018-05-13 21:19:59 -0700

    progress at home

--- vocabBot.py
+++ vocabBot.py
@@ -1,6 +1,5 @@
 import praw
 import apiCalls
-import json
 import hashlib
 
 class vocabBot:
@@ -8,42 +7,32 @@ class vocabBot:
     def __init__(self):
         self.redditInstance= praw.Reddit('vocab' , user_agent='Python/PC:vocabularyredditbot:v0.1')
 
-    def mentionsNamesToJson(self):
-        file= open("mentions.json","w+")
-        json.dump(self.mentionNames, file)
-
-    def jsonToMentionNames(self, mentionNames):
-        file=open("mentions.json", "r") 
-        mentionNames=json.load(file)
-
-    def getMentionNames(self, mentions):
-        mentionNames=[]
-        for comment in mentions:
-            mentionNames.append(comment.fullname)
-        return mentionNames
-
-    def getUserComments(self, username, commentLimit):
-        user= self.redditInstance.redditor(username)
-        if (commentLimit<1):
-            commentLimit=None
-        comments=[]
-        for comment in user.comments.new(limit=commentLimit):
-            comments.append(comment.body)
-        return comments
-
     def getMentions(self):
-        self.mentions=[]
         for mention in self.redditInstance.inbox.mentions(limit=None):
-            self.mentions.append(mention)
+            if mention.new:
+                replyToMention(mention)
+                mention.mark_read()
         return mentions
 
-    def replyToMention(self, mention, message):
+    def replyToMention(self, mention):
+        content=mention.body
+        parameters=content.split()
+        makeMessage(parameters)
         mention.reply(message)
 
-    def getCommentAuthor(self, comment)
-        author=comment.author
-        return author
+    #/u/vocabularyredditbot [word]  
+    def makeMessage(self,parameters)
 
-    def checkForNewMentions()
-        mentionsNames = getMentionNames(getMentions)
-        hash
+
+#####################
+unused code
+#####################
+
+    #def getUserComments(self, username, commentLimit):
+    #    user= self.redditInstance.redditor(username)
+    #   if (commentLimit<1):
+    #        commentLimit=None
+    #    comments=[]
+    #    for comment in user.comments.new(limit=commentLimit):
+    #        comments.append(comment.body)
+    #    return comments
